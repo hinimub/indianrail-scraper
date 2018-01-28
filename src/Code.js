4beda1f9acf0fb088673897931792f15813c68ba
hinimub <takumab21@gmail.com>
2018-01-28 15:28:58 +0530

    Update Code.js

--- src/Code.js
+++ src/Code.js
@@ -1,4 +1,4 @@
-function fare() {
+function fare(train, date, source, dest, class, quota) {
   var captchaUrl = 'http://www.indianrail.gov.in/enquiry/captchaDraw.png';
   var captchaRes = UrlFetchApp.fetch(captchaUrl);
   
@@ -7,7 +7,15 @@ function fare() {
   var headers = captchaRes.getAllHeaders();
   var cookies = headers['Set-Cookie'].map(function(str){return str.match(/.+;/)}).join('');
   var ans = solveCaptcha(captchaRes.getContent());
-  var url = 'http://www.indianrail.gov.in/enquiry/CommonCaptcha?inputCaptcha='+ans+'&trainNo=12559+-+SHIV+GANGA+EXP&dt=28-01-2018&sourceStation=MANDUADIH+-+MUV&destinationStation=NEW+DELHI+-+NDLS&classc=SL&quota=GN&inputPage=SEAT&language=en';
+  var url  = 'http://www.indianrail.gov.in/enquiry/CommonCaptcha';
+      url += '?inputCaptcha=' + ans;
+      url += '&trainNo=' + getTrainName(train);
+      url += '&dt=' + date;
+      url += '&sourceStation=' + getStationName(source);
+      url += '&destinationStation=' + getStationName(dest);
+      url += '&classc=' + class;
+      url += '&quota=' + quota;
+      url += '&inputPage=FARE&language=en';
   var options = {
     method: 'get',
     headers: {
